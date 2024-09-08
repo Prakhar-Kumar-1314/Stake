ebbe56b0e2e2b955a87d391a2a6c65b48e47c90c
Prakhar-Kumar-1314 <prakharkumar1314@gmail.com>
2024-09-09 00:27:56 +0530

    added basic signin and singup routing

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,22 +1,48 @@
 const { Router } = require("express");
 const {User} = require("../database/mongo");
-const {userExistsSignUp} = require("../middleware/user");
+const {jwt} = require("jsonwebtoken")
+const {userExistsSignUp, userValidation, userValidateSignin} = require("../middleware/user");
+const {JWT_SECRET_TOKEN} = require("../config");
 
 const userRouter = Router();
 
-userRouter.post("/signup", userExistsSignUp , async (req, res) => {
+userRouter.post("/signup", userExistsSignUp, userValidation , async (req, res) => {
     const {email, password, firstName, lastName} = req.body;
 
     try {
-        await User.create({
+        const user = await User.create({
             email, password, firstName, lastName
         })
+
         res.status(200).json({
-            message: "User created successfully"
+            message: "User created successfully",
         })
     } catch (err) {
         res.status(500).json({
             msg: "Error creating user",
+            error: err.message
+        })
+    }
+})
+
+userRouter.post("/signin", userValidateSignin, userValidation, async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+       const user = await User.findOne({ email, password });
+
+       if (user) {
+           res.status(200).json({
+               message: "Signed In!"
+           })
+       } else {
+           res.status(400).json({
+               message: "User does not exist, please signup"
+           })
+       }
+    } catch (err) {
+        res.status(400).json({
+            message: "Error while logging in",
             error: err.message
         })
     }
